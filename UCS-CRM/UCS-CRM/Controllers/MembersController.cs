073438520673dfce786eb6ab583e2f0ce67b6b77
James Simbi <james@sparcsystems.africa>
2023-02-22 12:09:12 +0200

    Initial Commit

---
+++ UCS-CRM/UCS-CRM/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UCS_CRM.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UCS-CRM/UCS-CRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UCS_CRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
