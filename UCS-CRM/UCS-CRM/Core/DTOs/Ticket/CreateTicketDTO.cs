d1734290f015eb15a19a3a10315d19a05323e846
James Simbi <james@sparcsystems.africa>
2023-03-16 16:49:08 +0200

    Allow nulls when creating a ticket by removing required attribute

--- UCS-CRM/UCS-CRM/Core/DTOs/Ticket/CreateTicketDTO.cs
+++ UCS-CRM/UCS-CRM/Core/DTOs/Ticket/CreateTicketDTO.cs
@@ -9,25 +9,25 @@ namespace UCS_CRM.Core.DTOs.Ticket
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
-        [Required]
+     
         [StringLength(maximumLength: 255)]
         [Display(Name = "Ticket Number")]
-        public string TicketNumber { get; set; }
+        public string? TicketNumber { get; set; }
         [Required]
         public string Description { get; set; }
-        [Required]
-        public string Status { get; set; }
+   
+        public string? Status { get; set; }
         public DateTime? ClosedDate { get; set; }
-        [Required]
+      
         [Display(Name = "Assigned To")]
-        public string AssignedToId { get; set; }
+        public string? AssignedToId { get; set; }
         [Required]
         [Display(Name = "Ticket Priority")]
         public int TicketPriorityId { get; set; }
         [Required]
         [Display(Name ="Ticket Category")]
         public int TicketCategoryId { get; set; }
-        [Required]
+    
         [Display(Name = "State")]
         public int? StateId { get; set; }
 
