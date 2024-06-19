4db0231d7e548ce22b26cd571564a4baefe553cd
Joseph Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2024-06-19 12:29:23 +0200

    removed unncessary date of birth assignment in ticket js files

--- UCS-CRM/UCS-CRM/wwwroot/js/ClerkTicket.js
+++ UCS-CRM/UCS-CRM/wwwroot/js/ClerkTicket.js
@@ -138,15 +138,7 @@ function EditForm(id, area = "") {
         type: 'GET'
     }).done(function (data) {
 
-        //get the input field inside the edit role modal form
-        //var date = new Date(data.dateOfBirth);
-        console.log(data)
-        var currentDate = new Date(data.dateOfBirth);
 
-        var day = ("0" + currentDate.getDate()).slice(-2);
-        var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-
-        var date = currentDate.getFullYear() + "-" + (month) + "-" + (day);
 
         const ticketFields = {
             Title: 'title',
